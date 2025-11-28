0383bf035cbcf69fb073f26a09203a07665dcef9
SultanaJahan25 <coursesellerbd25@gmail.com>
2025-11-28 17:28:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-const projectTrack = document.querySelector('#projects .carousel-track');
-const slides = document.querySelectorAll('.project');
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
+//About
 const aboutTrack = document.querySelector('#about-carousel .carousel-track');
 const aboutSlides = document.querySelectorAll('#about-carousel section');
 const aboutNext = document.getElementById('about-next');
 const aboutPrev = document.getElementById('about-prev');
 
-let index = 0;
+
 let aboutIndex = 0;
 
-//Projects
-function updateCarousel() {
-    projectTrack.style.transform = `translateX(-${index * 100}%)`;
-}
-nextBtn.addEventListener('click', () => {
-    if (index < slides.length - 1){
-        index++;
-        updateCarousel();
-    }
-});
-prevBtn.addEventListener('click', () => {
-    if(index > 0) {
-        index--;
-        updateCarousel();
-    }
-});
-
-//About
 function updateAboutCarousel() {
     const width = aboutSlides[0].offsetWidth;
     aboutTrack.style.transform = `translateX(-${aboutIndex * width}px)`;
@@ -50,3 +29,96 @@ aboutPrev.addEventListener('click', () => {
     updateAboutCarousel();
 });
 window.addEventListener('resize', updateAboutCarousel);
+
+//Projects
+const projectTrack = document.querySelector('#projects .carousel-track');
+const slides = document.querySelectorAll('#projects .project');
+const nextBtn = document.querySelector('#projects .next-btn');
+const prevBtn = document.querySelector('#projects .prev-btn');
+
+let index = 0;
+
+function updateCarousel() {
+    projectTrack.style.transform = `translateX(-${index * 100}%)`;
+}
+nextBtn.addEventListener('click', () => {
+    if (index < slides.length - 1){
+        index++;
+        updateCarousel();
+    }
+});
+prevBtn.addEventListener('click', () => {
+    if(index > 0) {
+        index--;
+        updateCarousel();
+    }
+});
+window.addEventListener('resize', updateCarousel);
+
+//Education
+/*const EduTrack = document.querySelector('#edu .edu-track');
+const EduSlides = document.querySelectorAll('.edu-slide');
+const EduNext = document.querySelector('.edu-next');
+const EduPrev = document.querySelector('.edu-prev');
+
+let EduIndex = 0;
+
+function updateEduCarousel() {
+    EduTrack.style.transform = `translateX(-${EduIndex * 100}%)`;
+}
+
+EduNext.addEventListener('click', () => {
+    if (EduIndex < EduSlides.length - 1) {
+        EduIndex++;
+    } else {
+        EduIndex = 0;
+    }
+        updateEduCarousel();
+});
+
+EduPrev.addEventListener('click', () => {
+    if (EduIndex > 0) {
+        EduIndex--;
+    } else {
+        EduIndex = EduSlides.length - 1;
+    }
+        updateEduCarousel();
+});
+window.addEventListener('resize', updateEduCarousel);*/
+
+//Certificate & Achievement
+document.addEventListener('DOMContentLoaded', () => {
+const CerTrack = document.querySelector('#certiAchiev .carousel-track');
+const CerNext = document.querySelector('#certiAchiev .cerNext');
+const CerPrev = document.querySelector('#certiAchiev .cerPrev');
+
+const maxSlides = 3;
+
+let CerIndex = 0;
+
+function updateCerCarousel() {
+    CerTrack.style.transform = `translateX( -${CerIndex * 100}% )`;
+}
+
+updateCerCarousel();
+
+CerNext.addEventListener('click', () => {
+    if (CerIndex < maxSlides - 1) {
+        CerIndex++;
+    } else {
+        CerIndex = 0;
+    }
+    updateCerCarousel();
+});
+
+CerPrev.addEventListener('click', () => {
+    if(CerIndex > 0) {
+        CerIndex--;
+    } else {
+        CerIndex = maxSlides - 1;
+    }
+    updateCerCarousel();
+});
+window.addEventListener('resize', updateCerCarousel);
+});
+
